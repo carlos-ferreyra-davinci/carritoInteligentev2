eaad91c9db6d71ccb6a0a01d0ea2b978ea2b0b0d
Carlos Ferreyra <carlos.ferreyra@davinci.edu.ar>
2024-11-26 23:44:00 -0300

    Creacion de modelos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import app from './app.js';
+import app from './app.js'
+import {connectDB} from "./db.js"
 
-app.listen(3000);
-console.log('server on port 3000');
+connectDB();
+app.listen(3100)
+console.log('server on');
